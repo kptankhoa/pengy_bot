5273ded5a5501c45495bac8ec27d28f63e4b2b6c
kptankhoa <kptankhoa@gmail.com>
2023-09-15 16:36:39 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { setUpBot } from 'libs/chatbot';
+import http from 'http';
 
 setUpBot();
 
 setInterval(() => {
   console.log('');
 }, 10 * 60 * 1000);
+
+const server = http.createServer();
+
+server.listen(3000);
